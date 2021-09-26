4d35e0889ead58b35177d5720a83e2b39939fefb
lion <breakermoob@hotmail.com>
2021-09-26 00:06:28 -0500

    Header Bootstrap styles

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,3 +1,56 @@
-export default function Root(props) {
-  return <section>{props.name} REACT!</section>;
+import "bootstrap/dist/css/bootstrap.css";
+import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+import { LinkContainer } from 'react-router-bootstrap';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+const Root = (props) => {
+  return <Router>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container className="d-flex align-items-center">
+        <LinkContainer to="/">
+          <Navbar.Brand className="d-flex align-items-center">
+            <i className="fab fa-react fa-2x" aria-hidden="true" />
+            <h2 style={{ margin: "0.5rem" }}>Microfrontends</h2>
+          </Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav">
+          <i className="fa fa-burger fa-lg" style={{ padding: "1rem 0.5rem" }} aria-hidden="true" />
+        </Navbar.Toggle>
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto">
+            <LinkContainer to="/perfil">
+              <Nav.Link>Perfil</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <Nav.Link>Ingresar</Nav.Link>
+            </LinkContainer>
+            <NavDropdown title="Ver más" id="collasible-nav-dropdown">
+              <LinkContainer to="/detalles">
+                <NavDropdown.Item>
+                  Panel - Subdominio
+                </NavDropdown.Item>
+              </LinkContainer>
+              <LinkContainer to="/blog">
+                <NavDropdown.Item>Blog - Path</NavDropdown.Item>
+              </LinkContainer>
+              <LinkContainer to="/perfil">
+                <NavDropdown.Item>Librería - NPM</NavDropdown.Item>
+              </LinkContainer>
+              <NavDropdown.Divider />
+              <LinkContainer to="/">
+                <NavDropdown.Item>Guía Completa</NavDropdown.Item>
+              </LinkContainer>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  </Router>
 }
+
+export default Root;
